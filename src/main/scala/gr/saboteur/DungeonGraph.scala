dc7b3aac30200aa2dcae5fddb33daa07e55bc9f9
glebreutov <glebr@algotradinghub.com>
2017-12-08 09:11:53 +0200

    some fixes

--- src/main/scala/gr/saboteur/DungeonGraph.scala
+++ src/main/scala/gr/saboteur/DungeonGraph.scala
@@ -1,6 +1,10 @@
 package gr.saboteur
 
 
+
+
+import scala.collection.immutable.HashMap
+import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 
 object CardEffect extends Enumeration{
@@ -36,33 +40,31 @@ object Ways extends Enumeration{
   }
 }
 
-
-class DungeonGraph (goldCardNum: Int){
+class DungeonGraph (goldPos: Int){
   val GRAPH_HEIGHT = 6
-  if (!(goldCardNum ==0 || goldCardNum == -2 || goldCardNum == 2)){
+
+  def card(pos: Int): DungeonCard = {
+    val artifact = if (pos == goldPos) CardEffect.GOLD else CardEffect.ORE
+    new DungeonCard(artifact, top = true)
+  }
+  if (!Set(0, -2, 2).contains(goldPos)){
     throw new RuntimeException("gold card should be in (-2, 0, 2)")
   }
-  val graph = scala.collection.mutable.Map((0, 0) -> new DungeonCard(CardEffect.START, bottom = true),
-    (GRAPH_HEIGHT, -2) -> new DungeonCard(CardEffect.ORE, top = true),
-    (GRAPH_HEIGHT, 0) -> new DungeonCard(CardEffect.ORE, top = true),
-    (GRAPH_HEIGHT, 2) -> new DungeonCard(CardEffect.ORE, top = true),
-  )
-  graph((GRAPH_HEIGHT, goldCardNum)) = new DungeonCard(CardEffect.GOLD, top = true)
+  val graph = mutable.Map((0, 0) -> new DungeonCard(CardEffect.START, bottom = true),
+    (GRAPH_HEIGHT, -2) -> card(-2),
+    (GRAPH_HEIGHT, 0) -> card(0),
+    (GRAPH_HEIGHT, 2) -> card(2))
 
-  def canAdd(pos: (Int, Int), card: DungeonCard): Boolean ={
-    val sbl = siblings(pos)
-    val (row, col) = pos
-    var ok = true
-    for((k, v) <- sbl){
-      ok = ok && card.place(k, graph(v))
-    }
 
-    !graph.contains(pos) && row > 0 && row <= GRAPH_HEIGHT && sbl.nonEmpty && ok
+  def fit(pos: (Int, Int), card: DungeonCard): Boolean ={
+    val sbl = neighbors(pos)
+    val (row, _) = pos
+    lazy val cardsFit = sbl.map(e => card.place(e._1, graph(e._2))).reduceLeft(_ && _)
+    !graph.contains(pos) && row > 0 && row <= GRAPH_HEIGHT && sbl.nonEmpty && cardsFit
   }
 
   def add(pos: (Int, Int), card: DungeonCard): Boolean ={
-    val can = canAdd(pos, card)
-    if(can){
+    if(fit(pos, card)){
       graph(pos) = card
       return true
     }else if(card.prop == CardEffect.BOOM){
@@ -72,7 +74,7 @@ class DungeonGraph (goldCardNum: Int){
     false
   }
 
-  def siblings(pos: (Int, Int), g: Map[(Int, Int), DungeonCard]=graph.toMap): Map[Ways.Value, (Int, Int)] = {
+  def neighbors(pos: (Int, Int), g: Map[(Int, Int), DungeonCard]=graph.toMap): Map[Ways.Value, (Int, Int)] = {
     val (row, col) = pos
     val cards = Map(Ways.LEFT -> (row, col - 1),
       Ways.RIGHT -> (row, col + 1),
@@ -90,7 +92,7 @@ class DungeonGraph (goldCardNum: Int){
     else if(g(coord).prop == CardEffect.GOLD){
       return true
     }else{
-      val sibleings = siblings(coord, g)
+      val sibleings = neighbors(coord, g)
       val graphTail = g - coord
       for((k, v) <- sibleings){
         if(goldFound(v, graphTail)){
